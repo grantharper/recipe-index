9cc3e613da80a16bd09d4488354f8854dcee2c6c
grantharper <grant.e.harper@gmail.com>
2017-10-11 07:44:05 -0400

    added ability to edit ingredients while editing a recipe

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 	
+	//initialize the ingredient buttons for editing an existing recipe
+	
+	setEditIngredients();
+	
 	$('#ingredient-input').keypress(function(e){
 	      if(e.keyCode==13)
 	      $('#ingredient-selection').click();
@@ -9,13 +13,13 @@ $(document).ready(function () {
 		
 		var ingredient = $('#ingredient-input').val();
 		//console.log('ingredient selected: ' + ingredient);
-		$('#picker-section').append('<button type="button" class="removebutton btn btn-default">' + ingredient + '</button>');
+		addIngredientButton(ingredient);
 		$('#ingredient-input').val('');
 		setIngredients();
 	});
 	
 	$('#picker-section').on('click', '.removebutton', function() {
-		console.log('remove button clicked');
+		//console.log('remove button clicked');
 		$(this).remove();
 		setIngredients();
 	});
@@ -25,6 +29,23 @@ $(document).ready(function () {
 	});
 	
 });
+
+function setEditIngredients(){
+	var ingredientList = $('#ingredient-list').val();
+	//console.log(ingredientList);
+	var ingredients = ingredientList.split(',');
+	
+	ingredients.forEach(function(ingredient){
+		ingredient = ingredient.trim();
+		//console.log(ingredient);
+		addIngredientButton(ingredient);
+	});
+	
+}
+
+function addIngredientButton(ingredient){
+	$('#picker-section').append('<button type="button" class="removebutton btn btn-default">' + ingredient + '</button>');
+}
 
 function setIngredients(){
 	//loop through all buttons in the picker section to determine the value of the hidden input
