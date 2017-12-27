be2dbba05e0a95640423c3ad7c1a252202691458
grantharper <grant.e.harper@gmail.com>
2017-12-27 07:39:08 -0500

    project lombok inclusion for domain objects

--- src/main/java/org/grantharper/recipe/model/Ingredient.java
+++ src/main/java/org/grantharper/recipe/model/Ingredient.java
@@ -11,95 +11,62 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-public class Ingredient implements Comparable<Ingredient>
-{
+public class Ingredient implements Comparable<Ingredient> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ingredient_id")
+    @Getter
+    @Setter
+    private Long id;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "ingredient_id")
-  private Long id;
-  
-  private String name;
-  
-  @ManyToMany(cascade = CascadeType.ALL, mappedBy = "ingredients")
-  private Set<Recipe> recipes = new TreeSet<>();
+    @Getter
+    @Setter
+    private String name;
 
-  public Long getId()
-  {
-    return id;
-  }
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "ingredients")
+    @Getter
+    @Setter
+    private Set<Recipe> recipes = new TreeSet<>();
 
-  public void setId(Long id)
-  {
-    this.id = id;
-  }
-
-  public String getName()
-  {
-    return name;
-  }
-
-  public void setName(String name)
-  {
-    this.name = name;
-  }
-
-  public Set<Recipe> getRecipes()
-  {
-    return recipes;
-  }
-
-  public void setRecipes(Set<Recipe> recipes)
-  {
-    this.recipes = recipes;
-  }
-  
-  
-
-  @Override
-  public String toString()
-  {
-    return "Ingredient [id=" + id + ", name=" + name + "]";
-  }
-
-  @Override
-  public int hashCode()
-  {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj)
-  {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Ingredient other = (Ingredient) obj;
-    if(other.getName().equalsIgnoreCase(this.getName())){
-      return true;
+    @Override
+    public String toString() {
+        return "Ingredient [id=" + id + ", name=" + name + "]";
     }
-    
-    return false;
-  }
 
-  @Override
-  public int compareTo(Ingredient other)
-  {
-    return this.getName().compareTo(other.getName());
-  }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
 
-  
-  
-  
-  
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Ingredient other = (Ingredient) obj;
+        if (other.getName().equalsIgnoreCase(this.getName())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int compareTo(Ingredient other) {
+        return this.getName().compareTo(other.getName());
+    }
+
 }
--- src/main/java/org/grantharper/recipe/model/Recipe.java
+++ src/main/java/org/grantharper/recipe/model/Recipe.java
@@ -13,111 +13,78 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-public class Recipe implements Comparable<Recipe>
-{
+public class Recipe implements Comparable<Recipe> {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "recipe_id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "recipe_id")
+    @Getter
+    @Setter
+    private Long id;
 
-  private String title;
+    @Getter
+    @Setter
+    private String title;
 
-  private Integer pageNumber;
+    @Getter
+    @Setter
+    private Integer pageNumber;
 
-  @ManyToMany(cascade = CascadeType.ALL)
-  @JoinTable(name = "recipe_ingredient", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
-  private Set<Ingredient> ingredients = new TreeSet<>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "recipe_ingredient", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+    @Getter
+    @Setter
+    private Set<Ingredient> ingredients = new TreeSet<>();
 
-  public Long getId()
-  {
-    return id;
-  }
-
-  public void setId(Long id)
-  {
-    this.id = id;
-  }
-
-  public String getTitle()
-  {
-    return title;
-  }
-
-  public void setTitle(String title)
-  {
-    this.title = title;
-  }
-
-  public Integer getPageNumber()
-  {
-    return pageNumber;
-  }
-
-  public void setPageNumber(Integer pageNumber)
-  {
-    this.pageNumber = pageNumber;
-  }
-
-  public Set<Ingredient> getIngredients()
-  {
-    return ingredients;
-  }
-
-  public void setIngredients(Set<Ingredient> ingredients)
-  {
-    this.ingredients = ingredients;
-  }
-  
-  
-
-  @Override
-  public String toString()
-  {
-    return "Recipe [id=" + id + ", title=" + title + ", pageNumber=" + pageNumber + "]";
-  }
-
-  @Override
-  public int hashCode()
-  {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    
-    result = prime * result + ((pageNumber == null) ? 0 : pageNumber.hashCode());
-    result = prime * result + ((title == null) ? 0 : title.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj)
-  {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Recipe other = (Recipe) obj;
-    if (id == null)
-    {
-      if (other.id != null)
-        return false;
-    } 
-    else if (id.equals(other.id))
-    {
-      return true;
+    @Override
+    public int compareTo(Recipe other) {
+        return this.getTitle().compareTo(other.getTitle());
     }
-   
-    return false;
 
-  }
+    @Override
+    public String toString() {
+        return "Recipe [id=" + id + ", title=" + title + ", pageNumber=" + pageNumber + "]";
+    }
 
-  @Override
-  public int compareTo(Recipe other)
-  {
-    return this.getTitle().compareTo(other.getTitle());
-  }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((pageNumber == null) ? 0 : pageNumber.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Recipe other = (Recipe) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (pageNumber == null) {
+            if (other.pageNumber != null)
+                return false;
+        } else if (!pageNumber.equals(other.pageNumber))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        return true;
+    }
 
 }
