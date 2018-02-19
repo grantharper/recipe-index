03b87275e24c45498bb8e53aad9b6146f8978c6a
grantharper <grant.e.harper@gmail.com>
2018-02-19 10:40:14 -0500

    add webjars for css, js dependencies

--- src/main/java/org/grantharper/recipe/config/SecurityConfiguration.java
+++ src/main/java/org/grantharper/recipe/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
   protected void configure(HttpSecurity httpSecurity) throws Exception {
 
     httpSecurity.authorizeRequests()/*.antMatchers("/console/**").permitAll()*/
-        .antMatchers("/css/**", "/health").permitAll()
+        .antMatchers("/health", "/webjars/**", "/css/**", "/js/**", "/img/**", "/favicon.png").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin().loginPage("/login").defaultSuccessUrl("/", true).permitAll().and()
