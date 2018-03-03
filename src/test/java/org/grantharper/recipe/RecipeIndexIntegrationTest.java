a40c7210f12517f9d29bae20fc39b6dc6d419a2e
grantharper <grant.e.harper@gmail.com>
2018-03-03 13:34:14 -0500

    add spock testing

--- src/test/java/org/grantharper/recipe/RecipeIndexIntegrationTest.java
+++ src/test/java/org/grantharper/recipe/RecipeIndexIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RecipeIndexApplicationTests {
+public class RecipeIndexIntegrationTest {
 
 	@Test
 	public void contextLoads() {
