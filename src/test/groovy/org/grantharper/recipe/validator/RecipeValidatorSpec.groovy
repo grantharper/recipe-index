0fafdd8acfc51cbdf236809db0cd79f27dbcd3db
grantharper <grant.e.harper@gmail.com>
2018-03-03 14:57:27 -0500

    convert validator test to parameterized spock test

--- src/test/groovy/org/grantharper/recipe/validator/RecipeValidatorSpec.groovy
+++ src/test/groovy/org/grantharper/recipe/validator/RecipeValidatorSpec.groovy
@@ -4,11 +4,10 @@ import spock.lang.*
 
 class RecipeValidatorSpec extends Specification {
     
+    RecipeValidator rv = new RecipeValidator();
+    
     @Unroll("Recipe title #input should be valid: #expected")
     def "validate title of recipe" () {
-        
-        given: "Recipe Validator"
-        RecipeValidator rv = new RecipeValidator();
         
         when: "user input is checked for valid characters"
         def result = rv.isValidTitle(input)
@@ -16,16 +15,52 @@ class RecipeValidatorSpec extends Specification {
         then: "appropriate determination is made for validity"
         result == expected
         
-        where: "following are potential input and expected result combinations"
+        where: "following are potential input and expected results"
         
-        input       || expected
-        "recipe"    || true
-        "cool, yeah"|| true
-        "<script>"  || false
-        
-        
+        input                                       || expected
+        "recipe"                                    || true
+        "beans, bread, milk"                        || true
+        "<script>"                                  || false
+        "hazelnut salad (with lots of dressing)"    || true
+        "<>[]{}"                                    || false
         
     }
     
+    @Unroll("Page number #input is valid number: #expected")
+    def "validate page numbers" () {
+        
+        when: "page number is provided by the user"
+        def result = rv.isValidPageNumber(input)
+        
+        then: "appropriate determination is made for validity"
+        result == expected
+        
+        where: "following are potential input and expected results"
+        
+        input       || expected
+        "96"        || true
+        "1a"        || false
+        "<"         || false
+        
+    }
+    
+    @Unroll("Ingredient #input is valid: #expected")
+    def "validate ingredient input" () {
+        
+        when: "ingredient is provided by the user"
+        def result = rv.isValidIngredients(input)
+        
+        then: "appropriate determination is made for validity"
+        result == expected
+        
+        where: "following are potential input and expected results"
+        
+        input                           || expected
+        "shallots; onions; apples"      || true
+        "pork, shoulder of; onions"     || true
+        "pork (boneless)"               || true
+        "pork < chicken"                || false
+        
+    }
     
 }
