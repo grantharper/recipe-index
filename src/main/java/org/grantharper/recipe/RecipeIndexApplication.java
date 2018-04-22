576c97153a103aac195c2fb9d7edca515f7cfe36
grantharper <grant.e.harper@gmail.com>
2018-04-22 13:41:29 -0400

    integrate local elasticsearch

--- src/main/java/org/grantharper/recipe/RecipeIndexApplication.java
+++ src/main/java/org/grantharper/recipe/RecipeIndexApplication.java
@@ -1,12 +1,23 @@
 package org.grantharper.recipe;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecipeIndexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeIndexApplication.class, args);
+	}
+
+	@Bean
+	RestHighLevelClient restHighLevelClient(){
+		return new RestHighLevelClient(
+						RestClient.builder(new HttpHost("localhost", 9200, "http"),
+										new HttpHost("localhost", 9201, "http")));
 	}
 }
---
+++ src/main/java/org/grantharper/recipe/controller/SearchController.java
@@ -0,0 +1,37 @@
+package org.grantharper.recipe.controller;
+
+import org.grantharper.recipe.domain.RecipePage;
+import org.grantharper.recipe.domain.RecipeSearch;
+import org.grantharper.recipe.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/elasticsearch")
+public class SearchController
+{
+
+  @Autowired
+  SearchService searchService;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String getIndex(Model model)
+  {
+    model.addAttribute("searchRecipe", new RecipeSearch());
+    return "elasticsearch";
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public String search(Model model, @ModelAttribute("searchRecipe") RecipeSearch recipeSearch)
+  {
+    List<RecipePage> recipePageList = searchService.searchElasticsearchForIngredients(recipeSearch.getSearchTerm());
+    model.addAttribute("searchResults", recipePageList);
+    return "elasticsearch";
+  }
+}
