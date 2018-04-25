d5ae7513a29803d0113e7d54a7810788f6492e02
grantharper <grant.e.harper@gmail.com>
2018-04-24 21:06:31 -0400

    add ingredient toggle switch

---
+++ src/main/resources/static/js/elasticsearch.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('.ingredient-display').click(function(){
+    toggleIngredientListDisplay($(this));
+  });
+});
+
+function toggleIngredientListDisplay(displayButton){
+  var displayed = false;
+  var td = displayButton.parent();
+  var styleAttr = $('div.ingredientList', td).attr('style');
+  if(styleAttr == 'display:visible;'){
+    displayed = true;
+  }
+  if(displayed){
+    console.log('make invisible');
+    $('div.ingredientList', td).attr('style', 'display:none;');
+    $('button', td).html('+');
+  }else{
+    console.log('make visible');
+    $('div.ingredientList', td).attr('style', 'display:visible;');
+    $('button', td).html('-');
+  }
+
+}
